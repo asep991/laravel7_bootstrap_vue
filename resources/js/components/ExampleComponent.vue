e87295ade5c81d11315add12f858aee5dbe36d07
asep991 <aseprdh99@gmail.com>
2020-03-06 22:46:10 +0700

    Initial Commit

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="container">
+      <b-jumbotron
+        header="Laravel_BootstrapVue"
+        lead="Laravel Bootstrap v4 Components for Vue.js 2"
+      >
+        <p>For more information visit website</p>
+        <b-button variant="primary" href="#">More Info</b-button>
+      </b-jumbotron>
+    </div>
+  </div>
+</template>
+
+
